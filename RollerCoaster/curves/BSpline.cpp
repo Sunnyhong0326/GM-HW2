337c4e126aba291249fc1082d85ffe06c098cb04
107062117 <sunnyhong91@gmail.com>
2025-03-20 22:08:36 +0800

    init

--- RollerCoaster/curves/BSpline.cpp
+++ RollerCoaster/curves/BSpline.cpp
@@ -13,4 +13,33 @@ void BSpline::build() {
   3. Otherwise, create the open uniform B-spline with m_points
      Note, the open uniform B-spline goes through p0 and pn
   */
+  Eigen::Matrix4f matM;
+  matM << -1.0f, 3.0f, -3.0f, 1.0f,
+      3.0f, -6.0f, 3.0f, 0.0f,
+      -3.0f, 0.0f, 3.0f, 0.0f,
+      1.0f, 4.0f, 1.0f, 0.0f;
+  matM = matM / 6;
+
+  int n = static_cast<int>(m_points.size());
+
+  if (m_loop) {
+      for (int i = 0; i < n; i++) {
+          addCurve(
+              m_points[(i - 1 + n) % n],  // P0
+              m_points[i % n],            // P1
+              m_points[(i + 1) % n],      // P2
+              m_points[(i + 2) % n],      // P3
+              matM);
+      }
+  }
+  else {
+      for (int i = 0; i < n-3; i++) {
+          addCurve(
+              m_points[i],          // P0
+              m_points[i + 1],      // P1
+              m_points[i + 2],      // P2
+              m_points[i + 3],      // P3
+              matM);
+      }
+  }
 }
--- RollerCoaster/curves/CatmullRom.cpp
+++ RollerCoaster/curves/CatmullRom.cpp
@@ -13,4 +13,61 @@ void CatmullRom::build() {
       3. Otherwise, create the open Catmull-Rom spline with m_points
          Note, the open Catmull-Rom spline goes through p0 and pn
     */
+    Eigen::Matrix4f matM, matMFL;
+    matM << -0.5f, 1.5f, -1.5f, 0.5f,
+        1.0f, -2.5f, 2.0f, -0.5f,
+        -0.5f, 0.0f, 0.5f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f;
+
+    matMFL << 2.0f, -2.0f, 1.0f, 1.0f,
+        -3.0f, 3.0f, -2.0f, -1.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        1.0f, 0.0f, 0.0f, 0.0f;
+    int n = static_cast<int>(m_points.size());
+
+    if (m_loop) {
+        // Closed Catmull-Rom Spline (Looping)
+        for (int i = 0; i < n; i++) {
+            addCurve(
+                m_points[(i - 1 + n) % n],  // P0
+                m_points[i % n],            // P1
+                m_points[(i + 1) % n],      // P2
+                m_points[(i + 2) % n],      // P3
+                matM);
+        }
+    }
+    else if (n > 3) {
+        // Open Catmull-Rom Spline
+
+        // First segment (H-spline)
+        addCurve(
+            m_points[0],
+            m_points[1],
+            m_points[1] - m_points[0],   // t0
+            (m_points[0] + m_points[2]) / 2,  // t1
+            matMFL
+        );
+
+        // Middle segments (Standard Catmull-Rom)
+        for (int i = 1; i < n - 3; i++) {
+            addCurve(
+                m_points[i],
+                m_points[i + 1],
+                m_points[i + 2],
+                m_points[i + 3],
+                matM
+            );
+        }
+
+        // Last segment (H-spline)
+
+        //addCurve(
+        //    m_points[n - 2],
+        //    m_points[n - 1],
+        //    (m_points[n - 2] + m_points[n]) / 2,  // tn-1
+        //    Eigen::Vector3f::Zero(),   // tn
+        //    matMFL
+        //);
+    }
+
 }
